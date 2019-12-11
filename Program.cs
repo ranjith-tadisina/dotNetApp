7840e75181f95071629b3d7f0147ea7ce3e34854
ranjith-tadisina <58748826+ranjith-tadisina@users.noreply.github.com>
2019-12-11 06:03:06 -0600

    Add files via upload
    All the source code attached here..

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.Odbc;
+
+using System.Data.SqlClient;
+
+namespace ConsoleDBConnApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OdbcConnection DbConnection = new OdbcConnection("DSN=odbc17;UID=sa;PWD=dbpwd1");
+            DbConnection.Open();
+            OdbcCommand DbCommand = DbConnection.CreateCommand();
+            DbCommand.CommandText = "SELECT * FROM test_tab";
+            OdbcDataReader DbReader = DbCommand.ExecuteReader();
+
+            int fCount = DbReader.FieldCount;
+            Console.Write(":");
+            for (int i = 0; i < fCount; i++)
+            {
+                String fName = DbReader.GetName(i);
+                Console.Write(fName + ":");
+            }
+            Console.WriteLine();
+
+
+            while (DbReader.Read())
+            {
+                Console.Write(":");
+                for (int i = 0; i < fCount; i++)
+                {
+                    String col = DbReader.GetString(i);
+
+                    Console.Write(col + ":");
+                }
+                Console.WriteLine();
+            }
+
+            DbReader.Close();
+            DbCommand.Dispose();
+            DbConnection.Close();
+
+
+
+
+
+
+
+        }
+    }
+}
